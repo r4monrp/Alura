8c7370af779cf138d8a975f0f6f2c6e225aebc7b
Ramon Alves Pereira <ramon.pereira@idealeware.com.br>
2017-12-12 14:01:14 -0200

    novas aulas alura

--- AspNetCore1.0/CasaDeCodigo/CasaDeCodigo/Startup.cs
+++ AspNetCore1.0/CasaDeCodigo/CasaDeCodigo/Startup.cs
@@ -43,7 +43,7 @@ namespace CasaDeCodigo
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
         }
     }
